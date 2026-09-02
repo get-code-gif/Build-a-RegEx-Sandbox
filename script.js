101d55c64eaf4ae4c9be096d3f54407194e84314
Getahun Alemayhu <getahune.alemayhu@gmail.com>
2026-09-02 20:52:31 +0300

    Add initial HTML structure, CSS styles, and JavaScript for Regex Sandbox functionality

---
+++ script.js
@@ -0,0 +1,21 @@
+const regexPattern = document.querySelector("#pattern");
+const stringToTest = document.querySelector("#test-string");
+const testButton = document.querySelector("#test-btn");
+const testResult = document.querySelector("#result");
+
+const caseInsensitiveFlag = document.querySelector("#i");
+const globalFlag = document.querySelector("#g");
+
+function getFlags() {
+  let flags = "";
+
+  if (caseInsensitiveFlag.checked) {
+    flags += "i";
+  }
+
+  if (globalFlag.checked) {
+    flags += "g";
+  }
+
+  return flags;
+}
